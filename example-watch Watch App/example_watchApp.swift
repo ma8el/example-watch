a4bde08a81ae0a79626bd8ed607af8328cd91d47
Markus Kurbel <m4rku@pm.me>
2023-04-25 21:05:53 +0200

    Initial Commit

---
+++ example-watch Watch App/example_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_watchApp.swift
+//  example-watch Watch App
+//
+//  Created by Markus Kurbel on 25.04.23.
+//
+
+import SwiftUI
+
+@main
+struct example_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
